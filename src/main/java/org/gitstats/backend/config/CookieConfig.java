f8735b053d1870f4e57e09b9885087b3f2a888ff
Abdul1028 <rasoolas2003@gmail.com>
2025-05-16 03:44:42 +0530

    ad cooki config

---
+++ src/main/java/org/gitstats/backend/config/CookieConfig.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+
+@Configuration
+public class CookieConfig {
+
+    @Bean
+    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+        // Set SameSite=None for all cookies and ensure Secure flag is used
+        return CookieSameSiteSupplier.ofNone().whenHasName("JSESSIONID");
+    }
+}
